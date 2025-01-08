899d11207285b1a720d10cbe2db15b1e20991876
lexaterra <lexaterra@proton.me>
2025-01-08 12:00:47 -0500

    Add files via upload

--- dj.sh
+++ dj.sh
@@ -1,11 +1,15 @@
 quit=""
+#mpd
+mpc --host=0.0.0.0 --port=8600 play
 while true; do
  
-    cmus-remote -n
-
+    #cmus-remote -n
+    mpc --host=0.0.0.0 --port=8600 next
+    mpc --host=0.0.0.0 --port=8600 seek 00:01:00
     read -n 1 -s -t $((59 - $(date +%s) % 60)) quit
     if [ -n "$quit" ]; then
     if [ $quit == "q" ]; then
+    mpc --host=0.0.0.0 --port=8600 stop
     exit
     fi
     else
